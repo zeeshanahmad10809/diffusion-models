3b8be05221bcd559d9d5f2b50fe3aca5b7c806fe
Zeeshan Ahmad <zeeshanahmad10809@gmail.com>
2023-06-06 10:41:21 +0200

    adding the files

---
+++ ex02_diffusion.py
@@ -0,0 +1,222 @@
+import torch
+import torch.nn.functional as F
+from ex02_helpers import extract
+from tqdm import tqdm
+
+
+def linear_beta_schedule(beta_start, beta_end, timesteps):
+    """
+    standard linear beta/variance schedule as proposed in the original paper
+    """
+    return torch.linspace(beta_start, beta_end, timesteps)
+
+
+# TODO: Transform into task for students
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+    """
+    # TODO (2.3): Implement cosine beta/variance schedule as discussed in the paper mentioned above
+    pass
+
+
+def sigmoid_beta_schedule(beta_start, beta_end, timesteps):
+    """
+    sigmoidal beta schedule - following a sigmoid function
+    """
+    # TODO (2.3): Implement a sigmoidal beta schedule. Note: identify suitable limits of where you want to sample the sigmoid function.
+    # Note that it saturates fairly fast for values -x << 0 << +x
+    pass
+
+
+class Diffusion:
+
+    # TODO (2.4): Adapt all methods in this class for the conditional case. You can use y=None to encode that you want to train the model fully unconditionally.
+
+    def __init__(self, timesteps, get_noise_schedule, img_size, device="cuda"):
+        """Diffusion Model
+
+        Parameters
+        ----------
+        timesteps : int
+            number of timesteps for the forward and reverse diffusion process
+        get_noise_schedule : method
+            takes timesteps as input and returns a noise tensor of shape (timesteps,)
+        img_size : int
+            image size (assuming square images)
+        device : str, optional
+            device to use for processing, by default "cuda"
+        """
+        self.timesteps = timesteps
+
+        self.img_size = img_size
+        self.device = device
+
+        # Note: In constructor, we're just calculating the required hyperparameters for forward and reverse diffusion process.
+
+        # define beta schedule
+        self.betas = get_noise_schedule(self.timesteps,) # shape (timesteps,)
+
+        # TODO: (2.2): Compute the central values for the equation in the forward pass already here so you can quickly use them in the forward pass.
+
+        # define alphas
+        self.alphas = 1 - self.betas # shape (timesteps,)
+        self.aphas_bar = torch.cumprod(self.alphas, axis=0) # shape (timesteps,). CAUTION: don't work without axis=o even though it's a 1D tensor
+        
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.sqrt_alpha_bar = torch.sqrt(self.alphas_bar) # shape (timesteps,)
+        self.sqrt_one_minus_alpha_bar = torch.sqrt(1 - self.alphas_bar) # shape (timesteps,)
+
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        self.alphas_bar_minus_1 = torch.cat((torch.tensor([0]), self.alphas_bar[:-1])) # prepend 0 for t=1. shape (timesteps+1,)
+        self.sqrt_betas = torch.sqrt(self.betas) # shape (timesteps,)
+        self.sqrt_recip_alphas = 1 / torch.sqrt(self.alphas) # shape (timesteps,)
+        self.sqrt_recip_one_minus_alphas_bar = 1 / torch.sqrt(1 - self.alphas_bar) # shape (timesteps+1,)
+
+
+    @torch.no_grad()
+    def p_sample(self, model, x, t, t_index):
+        """reverse diffusion process
+
+        Parameters
+        ----------
+        model : UNet
+            noise predictor
+        x : (batch_size, channels, img_size, img_size) tensor
+            image at timestep t
+        t : (batch_size,) tensor
+            randomly sampled timesteps
+        t_index : int
+            index of the current timestep
+
+        Returns
+        -------
+        posterior_mean : (batch_size, channels, img_size, img_size) tensor
+            mean image of the posterior distribution q(x_{t-1} | x_t, x_0)
+        """
+        # TODO: (2.2): implement the reverse diffusion process of the model for (noisy) samples x and timesteps t. Note that x and t both have a batch dimension
+        # Equation 11 in the paper
+        # Use our model (noise predictor) to predict the mean
+        # TODO: (2.2): The method should return the image at timestep t-1.
+
+
+        # Here we are going to predict the noise for the timestep t-1 using the model.
+        # Then to sample from the posterior distribution q(x_{t-1} | x_t, x_0) we need to
+        # sample from N(0, 1) and then scale and shift it using parameteres of posterior std and mean respectively.
+        # Remember: In IDDPM paper, we have equ. 10 and equ. 13 that can give us the posterior std and mean respectively.
+        # std. have a closed-form solution but mean needs to be estimated using the reparameterization trick and predicting
+        # the noise at the timestep t-1 using the model.
+
+
+        # Step-01: Compute variance of the posterior distribution q(x_{t-1} | x_t, x_0) anaytically using equ. 10 in the paper.
+        posterior_betas = ((1 - self.alphas_bar_minus_1) / (1 - self.alphas_bar)) * self.betas # shape (timesteps,)
+
+        # Step-02: Compute the mean of the posterior distribution q(x_{t-1} | x_t, x_0) using the reparameterization trick and equ. 13 in the paper.
+        posterior_mean = extract(self.sqrt_recip_alphas, t, x.shape) *\
+            (x - (extract(self.betas, t, x.shape) * model(x, t)) /\
+            extract(self.sqrt_recip_one_minus_alphas_bar, t, x.shape)) # shape: (batch_size, channels, img_size, img_size)
+        
+        # Step-03: Sample from the posterior distribution q(x_{t-1} | x_t, x_0) using the reparameterization trick.
+        if t_index == 0:
+            # simply return the mean of the posterior distribution q(x_{t-1} | x_t, x_0) for t=0. Don't sample.
+            return posterior_mean # shape: (batch_size, channels, img_size, img_size)
+        else:
+            # Here, we scale and shift the standard normal distribution N(0, 1) using the mean and std of the
+            # posterior distribution q(x_{t-1} | x_t, x_0), and return the sample x_{t-1} ~ q(x_{t-1} | x_t, x_0).
+            return posterior_mean + extract(self.sqrt_betas, t, x.shape) * torch.randn_like(x, device=self.device) # shape: (batch_size, channels, img_size, img_size)
+
+    # Algorithm 2 (including returning all images)
+    @torch.no_grad()
+    def sample(self, model, image_size, batch_size=16, channels=3):
+        """sample from the model
+
+        Parameters
+        ----------
+        model : UNet
+            noise predictor
+        image_size : int
+            image size (assuming square images)
+        batch_size : int, optional
+            batch size, by default 16
+        channels : int, optional
+            number of channels, by default 3
+
+        Returns
+        -------
+        imgs_at_t : list of (batch_size, channels, img_size, img_size) tensors
+            list of images at each timestep
+        """
+        # TODO: (2.2): Implement the full reverse diffusion loop from random noise to an image, iteratively ''reducing'' the noise in the generated image.
+        # TODO: (2.2): Return the generated images
+
+        # Notice: We here in step-02 iterate back over all of the T timesteps, but in the step-02.1: we sample
+        # a random timestep t ~ U(0, T) for each sample in the batch.
+        # Maybe this randomness avoids the Unet to overfit to the sequence of images and instead it learns to
+        # generate noise for each timestep independently.
+
+        # Step-01: Sample a random noise vector z_T ~ N(0, 1) of shape (batch_size, channels, img_size, img_size)
+        x_T = torch.randn(batch_size, channels, image_size, image_size, device=self.device)
+
+        # Step-02: Iterate over all timesteps in the reverse diffusion process
+        x_t = x_T
+        imgs_at_t = []
+        for step_t in tqdm(reversed(range(self.timesteps)), desc="reverse diffusion process", total=self.timesteps):
+            # Step-02.1: create a batch of same timestep t for all samples in the batch
+            t = torch.tensor([step_t] * batch_size, device=self.device).long() # no need to use long() here, but just to be sure
+            x_t = self.p_sample(model, x_t, t, t_index=step_t) # shape: (batch_size, channels, img_size, img_size)
+            imgs_at_t.append(x_t)
+
+        # Step-03: Return the generated images
+        return imgs_at_t # shape: [(batch_size, channels, img_size, img_size), ...]
+
+    # forward diffusion (using the nice property)
+    def q_sample(self, x_zero, t, noise=None):
+        """forward diffusion process
+
+        Parameters
+        ----------
+        x_zero : (batch_size, channels, img_size, img_size) tensor
+            image at t=0
+        t : (batch_size,) tensor
+            t ~ U(0, T) where T is the number of timesteps
+        noise : (batch_size, channels, img_size, img_size) tensor, optional
+             standard normal noise vector, by default None
+
+        Returns
+        -------
+        x_t : (batch_size, channels, img_size, img_size) tensor
+            noised images at timestep t
+        """
+        # TODO: (2.2): Implement the forward diffusion process using the beta-schedule defined in the constructor; if noise is None, you will need to create a new noise vector, otherwise use the provided one.
+        # Notice we sample the noise vector from a standard normal distribtuion of the same lenght as the image vector
+        # and we don't add a single noise constant value because may just shift the color but don't change the distribution
+        # of the image.
+        if not noise:
+            noise = torch.randn_like(x_zero, device=self.device)
+
+        # extract method extracts the value of forward and diffusion parameters for a particular timestep
+        # provided in the batch. For example, if alpha = [0.1, 0.2, 0.3, 0.4], alpha.shape: (timesteps,)
+        # t = [1, 2], t.shape: (batch_size,), then extract(alpha, t, x_zero.shape) will return [0.2, 0.3],
+        # however, it will reshape the [0.2, 0.3] it to (batch_size, 1, 1, 1) to match the shape of x_zero.
+        # So the output will be [[[[0.2]]], [[[0.3]]]].
+        # Notice, we have only a constant value for each image and not separate value for each channel or pixel.
+        # but how does it destroy the image by adding only a constant value to it over time?
+        sqrt_alpha_bar_t_prod_x_zero = extract(self.sqrt_alpha_bar, t, x_zero.shape) * x_zero
+        sqrt_one_minus_alpha_bar_t_prod_noise = extract(self.sqrt_one_minus_alpha_bar, t, noise.shape) * noise
+        x_t = sqrt_alpha_bar_t_prod_x_zero + sqrt_one_minus_alpha_bar_t_prod_noise
+        return x_t
+
+    def p_losses(self, denoise_model, x_zero, t, noise=None, loss_type="l1"):
+        # TODO (2.2): compute the input to the network using the forward diffusion process and predict the noise using the model; if noise is None, you will need to create a new noise vector, otherwise use the provided one.
+
+        if loss_type == 'l1':
+            # TODO (2.2): implement an L1 loss for this task
+            loss = None
+        elif loss_type == 'l2':
+            # TODO (2.2): implement an L2 loss for this task
+            loss = None
+        else:
+            raise NotImplementedError()
+
+        return loss
---
+++ ex02_model.py
@@ -0,0 +1,363 @@
+import math
+from functools import partial
+from einops import rearrange, reduce
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+from ex02_helpers import *
+
+
+# Note: This code employs large parts of the following sources:
+# Niels Rogge (nielsr) & Kashif Rasul (kashif): https://huggingface.co/blog/annotated-diffusion (last access: 23.05.2023),
+# which is based on
+# Phil Wang (lucidrains): https://github.com/lucidrains/denoising-diffusion-pytorch (last access: 23.05.2023)
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+
+
+class WeightStandardizedConv2d(nn.Conv2d):
+    """
+    https://arxiv.org/abs/1903.10520
+    weight standardization purportedly works synergistically with group normalization
+    """
+    # Note: Here we're inheriting from nn.Conv2d because we want to modify the
+    # weight initialization, that's why we use F.conv2d(...) to provide the custom
+    # filter weights to the forward pass
+
+    def forward(self, x):
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+
+        weight = self.weight
+        mean = reduce(weight, "o ... -> o 1 1 1", "mean")
+        var = reduce(weight, "o ... -> o 1 1 1", partial(torch.var, unbiased=False))
+        normalized_weight = (weight - mean) * (var + eps).rsqrt()
+
+        return F.conv2d(
+            x,
+            normalized_weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+
+# Here try to think intuitvely about the group-normalization. What does it do?
+# you can look at it at: https://theaisummer.com/normalization/
+class Block(nn.Module):
+    def __init__(self, dim, dim_out, groups=8):
+        super().__init__()
+        self.proj = WeightStandardizedConv2d(dim, dim_out, 3, padding=1)
+        self.norm = nn.GroupNorm(groups, dim_out)
+        self.act = nn.SiLU()
+
+    # Note: This implements FiLM conditioning, see https://distill.pub/2018/feature-wise-transformations/ and
+    # http://arxiv.org/pdf/1709.07871.pdf
+    def forward(self, x, scale_shift=None):
+        x = self.proj(x)
+        x = self.norm(x)
+
+        if exists(scale_shift):
+            scale, shift = scale_shift
+            x = x * (scale + 1) + shift
+
+        x = self.act(x)
+        return x
+
+
+class ResnetBlock(nn.Module):
+    """https://arxiv.org/abs/1512.03385"""
+
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, classes_emb_dim=None, groups=8):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(int(time_emb_dim) + int(classes_emb_dim), dim_out * 2) # dim_out*2 because we need to separate scale and shift
+        ) if exists(time_emb_dim) or exists(classes_emb_dim) else None
+
+        self.block1 = Block(dim, dim_out, groups=groups)
+        self.block2 = Block(dim_out, dim_out, groups=groups)
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, time_emb=None, class_emb=None):
+        # time_emb: (b, dim*4), class_emb: (b, dim_in*4). dim is different from dim_in and dim_out
+        # dim is the dimension of the intermediate channels somewhat similar to dimension in the transformer.
+
+        scale_shift = None
+        if exists(self.mlp) and (exists(time_emb) or exists(class_emb)):
+            cond_emb = tuple(filter(exists, (time_emb, class_emb)))
+            cond_emb = torch.cat(cond_emb, dim=-1) # shape: (b, dim*4 + dim*4). concatenate time_emb and class_emb
+            cond_emb = self.mlp(cond_emb)
+            cond_emb = rearrange(cond_emb, 'b c -> b c 1 1') # shape: (b, dim_out*2, 1, 1)
+            scale_shift = cond_emb.chunk(2, dim=1) # separate scale and shift
+
+        h = self.block1(x, scale_shift=scale_shift) # x: (b, dim, h, w), scale_shift: ((b, dim_out, 1, 1), (b, dim_out, 1, 1))
+        # Here x is (b, dim, h, w) but in Block's forward pass, it is projected to (b, dim_out, h, w). Then we can shift and scale
+
+        h = self.block2(h)
+
+        return h + self.res_conv(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super().__init__()
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False) # 1x1 convolution to map the input to q, k, v
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1) # 1x1 convolution to map the output to the original dimension
+
+    def forward(self, x):
+        # Here dim_head*heads*3 is encoded in the form of channels. Isn't it interesting?
+        b, c, h, w = x.shape
+        # .chunk() split the qkv tensor into 3 parts for q, k, v in a tuple
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        # we map the tuple to transform tensor of k, q, v into shape (b, heads*dim_head, h, w)
+        # rearrange() is used to rearrange to shape (b, heads, dim_head, h*w). h*w is the number of pixels in the image
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+        q = q * self.scale
+
+        # q, k, v are now of shape (b, heads, dim_head, h*w) # dim_head for each pixel, and total number of pixels is h*w
+        # here we compute similiarity between each pixel in the image, however, in ViT
+        # we compute similiarity between each token/patch in the sequence
+        sim = einsum("b h d i, b h d j -> b h i j", q, k) # sim: (b, heads, h*w, h*w)
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach() # Here we normalize the similiarity matrix by subtracting the max value
+        attn = sim.softmax(dim=-1) # attn: (b, heads, h*w, h*w)
+
+        # it will automaticaly transpose the v tensor to shape (b, heads, h*w, dim_head)
+        out = einsum("b h i j, b h d j -> b h i d", attn, v) # out: (b, heads, h*w, dim_head)
+        out = rearrange(out, "b h (x y) d -> b (h d) x y", x=h, y=w) # out: (b, heads*dim_head, h, w)
+        return self.to_out(out) # out: (b, dim, h, w)
+
+
+# Linear attention variant, scales linear with sequence length
+# Shen et al.: https://arxiv.org/abs/1812.01243
+# https://github.com/lucidrains/linear-attention-transformer
+# Above we have implemented dot-product attention, here we're going to look a variant of
+# attention known as efficient attention, which is considered to be more computationally, and
+# specially memory efficient. Mathematically, it is equivalent to the dot-product attention, however,
+# it grows linearly with the sequence length, instead of quadratically. For details, please refer to
+# arxiv paper that explains how the projection/softmax is differently computed in this case.
+class LinearAttention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super().__init__()
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+
+        self.to_out = nn.Sequential(nn.Conv2d(hidden_dim, dim, 1),
+                                    nn.GroupNorm(1, dim))
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+
+        # Here q, k, v are of shape (b, heads, dim_head, h*w)
+        # where h*w is the number of input pixels, and dim_head is the dimension of each pixel
+        # To better understand the dim=-2, dim=-1 please refer the Figure 1 in the paper
+        # mentioned above.
+        q = q.softmax(dim=-2) # Here we compute softmax along the second-last dimension
+        k = k.softmax(dim=-1) # Here we compute softmax along the last dimension
+
+        q = q * self.scale
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
+
+        out = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+        out = rearrange(out, "b h c (x y) -> b (h c) x y", h=self.heads, x=h, y=w)# shape: (b, dim_head, h, w)
+        return self.to_out(out)
+
+
+# Wu et al.: https://arxiv.org/abs/1803.08494
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.GroupNorm(1, dim) # apply group normalization along the channel dimension considering
+        # the all channels as single group (equivalent to layer normalization), however, we can also group the 
+        # channels into 2 or more groups, and apply group normalization along each group. Remember, the output
+        # is going to be of same shape as the input regardless of the number of groups we choose.
+        # For more details, please refer to the following link: https://theaisummer.com/normalization/
+        # (h, w) along perpendicular z-axis, number of channels along y-axis, and batch size along x-axis
+
+    def forward(self, x):
+        x = self.norm(x)
+        return self.fn(x)
+
+
+# TODO: make yourself familiar with the code that is presented here, as it closely interacts with the rest of the exercise.
+class Unet(nn.Module):
+    def __init__(
+        self,
+        dim,
+        init_dim=None,
+        out_dim=None,
+        dim_mults=(1, 2, 4, 8),
+        channels=3,
+        resnet_block_groups=4,
+        class_free_guidance=False,  # TODO: Incorporate in your code
+        num_classes=None,
+        p_uncond=None,
+    ):
+        super().__init__()
+
+        # determine dimensions
+        self.channels = channels
+        input_channels = channels   # adapted from the original source
+
+        init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv2d(input_channels, init_dim, 1, padding=0)  # changed to 1 and 0 from 7,3
+
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)] # dims = [init_dim, dim, dim*2, dim*4, dim*8].
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        block_klass = partial(ResnetBlock, groups=resnet_block_groups)
+
+        # time embeddings
+        # dim * 4 has nothing to do with the number of channels in the input image, it is just chosen arbitrarily in the
+        # original implementation.
+        time_dim = dim * 4 
+
+        self.time_mlp = nn.Sequential(
+            SinusoidalPositionEmbeddings(dim),
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim),
+        ) # shape: (b, dim*4)
+
+        # TODO: Implement a class embedder for the conditional part of the classifier-free guidance & define a default
+        self.class_free_guidance = class_free_guidance
+        class_dim = None
+        if self.class_free_guidance:
+            class_dim = dim * 4
+            self.p_uncond = default(p_uncond, 0.1)
+            self.class_mlp = nn.Sequential(
+                nn.Dropout(p=self.p_uncond),
+                nn.Embedding(num_classes, class_dim),
+                nn.Linear(class_dim, class_dim),
+                nn.GELU(),
+                nn.Linear(class_dim, class_dim),
+            ) # shape: (b, dim*4)
+        
+
+        # layers
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        # TODO: Adapt all blocks accordingly such that they can accommodate a class embedding as well
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim, classes_emb_dim=class_dim),
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim, classes_emb_dim=class_dim),
+                        Residual(PreNorm(dim_in, LinearAttention(dim_in))), # Residual is in helpers.py. It simpley creates a residual connection.
+                        Downsample(dim_in, dim_out)
+                        if not is_last
+                        else nn.Conv2d(dim_in, dim_out, 3, padding=1),
+                    ]
+                )
+            )
+
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim, classes_emb_dim=class_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim))) # Notice here we're using Attention instead of LinearAttention
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim, classes_emb_dim=class_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1)
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim, classes_emb_dim=class_dim), # dim_out + dim_in because we're concatenating the skip connection
+                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim, classes_emb_dim=class_dim),
+                        Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                        Upsample(dim_out, dim_in)
+                        if not is_last
+                        else nn.Conv2d(dim_out, dim_in, 3, padding=1),
+                    ]
+                )
+            )
+
+        self.out_dim = default(out_dim, channels) # out_dim is the number of channels in the output image
+
+        self.final_res_block = block_klass(dim * 2, dim, time_emb_dim=time_dim, classes_emb_dim=class_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time, class_cond=None):
+
+        x = self.init_conv(x)
+        r = x.clone() # clone here for the final residual connection
+
+        t = self.time_mlp(time) # shape: (b, dim*4)
+
+        # TODO: Implement the class conditioning. Keep in mind that
+        #  - for each element in the batch, the class embedding is replaced with the null token with a certain probability during training
+        #  - during testing, you need to have control over whether the conditioning is applied or not
+        #  - analogously to the time embedding, the class embedding is provided in every ResNet block as additional conditioning
+        if self.class_free_guidance:
+            class_cond = F.one_hot(class_cond, num_classes=10).float()
+            class_cond = self.class_mlp(class_cond) # shape: (b, dim*4)
+
+        h = []
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t, class_cond)
+            h.append(x)
+
+            x = block2(x, t, class_cond)
+            x = attn(x)
+            h.append(x)
+
+            x = downsample(x)
+
+        x = self.mid_block1(x, t, class_cond)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t, class_cond)
+
+        for block1, block2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block1(x, t, class_cond)
+
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t, class_cond)
+            x = attn(x)
+
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1) # r is the residual connection that we stored earlier
+
+        x = self.final_res_block(x, t, class_cond)
+        return self.final_conv(x)
+
+
+if __name__ == "__main__":
+    x = torch.randn(1, 3, 32, 32)
+    time = torch.randn(1, 32)
+    class_cond = torch.randint(0, 10, (1,))
+    unet = Unet(32, channels=3)
+    print(unet(x, time).shape)
+    unet1 =  Unet(32, channels=3, class_free_guidance=True, num_classes=10, p_uncond=0.1)
+    print(unet1(x, time, class_cond).shape)
